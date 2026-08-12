f0d25d066241420b11699c7c2d04003d194e7cbf
Dean Fox <deanfoxcd@gmail.com>
2026-08-12 17:46:18 -0400

    Laid out main components, refactored search, added localization

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
-import MovieSearch from "@/components/Search";
+import { ButtonSolid } from "@/components/blocks/ButtonSolid";
+import { Search } from "@/components/blocks/Search";
+import { Stack, Typography } from "@mui/material";
 
 export default function Home() {
-  return <MovieSearch />;
+  return (
+    <Stack spacing={3}>
+      <Typography variant="h1" align="center">
+        Physical Media Tracker
+      </Typography>
+      <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
+        <ButtonSolid>My Collection</ButtonSolid>
+        <ButtonSolid>Wishlist</ButtonSolid>
+      </Stack>
+      <Stack spacing={1}>
+        <Typography>Find a movie or show:</Typography>
+        <Search />
+      </Stack>
+    </Stack>
+  );
 }
